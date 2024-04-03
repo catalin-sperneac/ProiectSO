78f6ceb3d8635a9bcbe0e75a9103ef9942378802
Student <student@b528b-15.intranet.cs.upt.ro>
2024-04-03 11:49:08 +0300

    S7L

--- proiect.c
+++ proiect.c
@@ -6,29 +6,35 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-FILE *snapshot;
+//FILE *snapshot;
 
 // functie pentru deschiderea fisierului 
-void openFile(char *path) 
+int openFile(char *path,char *file) 
 {
     char snapshotPath[1024];
-    sprintf(snapshotPath, "%s/snapshot.txt", path);
-    snapshot=fopen(snapshotPath, "wt");
-    if (snapshot == NULL) {
+    sprintf(snapshotPath, "%s/%s", path,file);
+    //int fd=open(snapshotPath, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+    int fd=creat(snapshotPath,S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+    if (fd == -1) 
+    {
         perror("Eroare deschidere fisier snapshot.txt pentru scriere!\n");
-        exit(EXIT_FAILURE);
+        exit(0);
     }
+    return fd;
 }
 
 //functie pentru parcurgerea recusiva a unui director pentru a obtine informatii despre acesta
 //path - calea catre director
 //depth - nivelul de adancime in director
-void listFiles(char *path,int depth) 
+void listFiles(char *path,char *file,int depth) 
 {
     DIR *dir;
     struct dirent *entry;
     struct stat file_info;
+    //deschidem fisierul
+    int fd=openFile(path,file);
     //deschidem directorul
     if (!(dir = opendir(path))) 
     {
@@ -45,11 +51,24 @@ void listFiles(char *path,int depth)
         {
             continue;
         }
+        char *buffer="";
         for (int i = 1; i < depth; ++i)
         {
-            fprintf(snapshot,"|  ");
+            //char *buffer="";
+            strcat(buffer,"|  ");
+            //write(fd,buffer,strlen(buffer));
+            //fprintf(snapshot,"|  ");
         }
-        fprintf(snapshot,"|_ %s\n",entry->d_name);
+        //char *buffer2="";
+        strcat(buffer,"|_ ");
+        strcat(buffer,entry->d_name);
+        strcat(buffer,"\n");
+        if((write(fd,buffer,strlen(buffer)))<0)
+        {
+            perror("Eroare write\n");
+            exit(0);
+        }
+        //fprintf(snapshot,"|_ %s\n",entry->d_name);
         //obtinem informatii despre fisierul/directorul curent
         if (stat(filepath, &file_info) == -1) 
         {
@@ -59,11 +78,12 @@ void listFiles(char *path,int depth)
         //daca intrarea este un director, se va apela recursiv functia listFiles cu un nivel de adancime mai mare
         if (S_ISDIR(file_info.st_mode)) 
         {
-            listFiles(filepath,depth+1);
+            listFiles(filepath,file,depth+1);
         }
     }
     //inchidem directorul
     closedir(dir);
+    close(fd);
 }
 
 int main(int argc,char *argv[]) 
@@ -73,9 +93,9 @@ int main(int argc,char *argv[])
         perror("Eroare numar de argumente!\n");
         exit(0);
     }
-    openFile(argv[1]);
-    fprintf(snapshot,"%s\n",argv[1]);
-    listFiles(argv[1],1);
-    fclose(snapshot);
+    char *snapshot="snapshot_";
+    strcat(snapshot,argv[1]);
+    strcat(snapshot,".txt");
+    listFiles(argv[1],snapshot,1);
     return 0;
 }
